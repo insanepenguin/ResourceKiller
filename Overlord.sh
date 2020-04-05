02fc5997ef7a11a0e66afdf43bd8b9f901bd591e
Student <student@localhost.localdomain>
2020-04-05 11:48:33 -0400

    Added one loop to rule them all, Built Kill and Run Functions

--- Overlord.sh
+++ Overlord.sh
@@ -7,19 +7,30 @@ FileCheck "df.txt"
 FileCheck "ifstat.txt"
 FileCheck "ps.txt"
 }
-function StartAPMS() {
-start "APM1" &
-start "APM2" &
-start "APM3" &
-start "APM4" &
-start "APM5" &
-start "APM6" &
+# function StartAPMS() {
+# start "APM1" &
+# start "APM2" &
+# start "APM3" &
+# start "APM4" &
+# start "APM5" &
+# start "APM6" &
+# }
+function RunC() {
+	./APM1 192.168.195.128 &
+	./APM2 192.168.195.128 &
+	./APM3 192.168.195.128 &
+	./APM4 192.168.195.128 &
+	./APM5 192.168.195.128 &
+	./APM6 192.168.195.128 &
 }
 function StartCollection() {
-
-}
-function Purge() {
-
+	bash ps.sh
+	bash ifstat.sh
+} 
+function Kill() {
+	killall APM*
+	Kill ps.sh
+	Kill ifstat.sh
 }
 
 function FileCheck() {
@@ -27,11 +38,21 @@ if test -f "$1"; then
 	rm "$1"
 fi
 }
+RunC
+count = 1
+while [ $count -le 16 ];
+do
+	#Measure here ifstat every 1, PS every 5 
+	StartCollection
+	
+	echo "$count"
+	(($count++))
+done
+Kill
+# iostat >> iostat.txt
+# df >> df.txt
 
-iostat >> iostat.txt
-df >> df.txt
-
-sleep 65
+# sleep 65
 
 #gets line but now specify cpu and mem %s
 cat ps.txt | grep "APM1" >> APM1_metrics.csv
--- ifstat.sh
+++ ifstat.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 pidof ifstat.sh >> pid.txt
-while [ $SECONDS -le 30 ]; do
-	ifstat >> ifstat.txt
-	echo "****************************************************************" >> ifstat.txt
-	sleep 1
-done
-
+# while [ $SECONDS -le 30 ]; do
+# 	ifstat >> ifstat.txt
+# 	echo "****************************************************************" >> ifstat.txt
+# 	sleep 1
+# done
+ifstat >> ifstat.txt
+echo "****************************************************************" >> ifstat.txt
