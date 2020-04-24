4b0070ec0709d74bdbd2152184c44ca9f856c2f3
Elijah Heilman <54540869+Eoh2888@users.noreply.github.com>
2020-04-23 23:57:01 -0400

    Fixed everything even timing. Needs additional testing. Maybe get rid of ending errors and check if info looks good in csvs.

--- Overlord.sh
+++ Overlord.sh
@@ -7,27 +7,30 @@ date > iostat.txt
 date > ifstat.txt
 date > df.txt
 date > System_Metrics.csv
-echo "Seconds, RX TX, Disk Write, Available Space" >> System_Metrics.csv
+echo "Seconds, RX, TX, Disk Write, Available Space" >> System_Metrics.csv
 
 #Globally declare variables to use.
 $seconds
 $RX_TX
+$RX
+$TX
 $Disk_write
 $Avail_Space
 $toWrite
-
-function Prep() {
-FileCheck "pid.txt"
-FileCheck "iostat.txt"
-FileCheck "df.txt"
-FileCheck "ifstat.txt"
-FileCheck "ps.txt"
-}
+$ens
+ens=`ifconfig | awk '{print $1}' | head -1 | sed 's/://g'`
 
 #Made all of the data collection be assigned to a variable and then be written to a file (System_Metrics.csv)
 function collect() {
 	seconds=$SECONDS
-	ps -a -o cmd, -o %cpu, -o %mem | grep "..APM*" >> Process.csv
+	#ps -a -o cmd, -o %cpu, -o %mem | grep "..APM*" >> APM_Metrics.csv
+	ps -a -o cmd, -o %cpu, -o %mem | grep "..APM1" >> APM1_Metrics.csv
+	ps -a -o cmd, -o %cpu, -o %mem | grep "..APM2" >> APM2_Metrics.csv
+	ps -a -o cmd, -o %cpu, -o %mem | grep "..APM3" >> APM3_Metrics.csv
+        ps -a -o cmd, -o %cpu, -o %mem | grep "..APM4" >> APM4_Metrics.csv
+	ps -a -o cmd, -o %cpu, -o %mem | grep "..APM5" >> APM5_Metrics.csv
+        ps -a -o cmd, -o %cpu, -o %mem | grep "..APM6" >> APM6_Metrics.csv
+
 	#Find gets the % mem and % CPU of each process running on the system the filters for the APM files
 	Disk_Write=`iostat -d sda | awk '{print $4}' | tail -2`
 	#
@@ -35,15 +38,19 @@ function collect() {
 	Avail_Space=`df -BM / | awk '{print $4}' | sed  's/M//g' | tail -1`
 	#
 	#echo $Avail_Space
-	RX_TX=`ifstat --interval=1 ens33 | awk '{ print $3, $5 }' | tail -2`
-	#echo $RX_TX
+	RX_TX=`ifstat --interval=1 $ens | awk '{ print $6, $7, $8, $9 }' | tail -2 | head -1 | awk '{print $1,$3}'`
+	RX=`echo $RX_TX | awk '{print $1}' | sed  's/K/000/g'`
+	TX=`echo $RX_TX | awk '{print $2}' | sed  's/K/000/g'`
+        #echo $RX_TX
 	#Finds the TX and RX data rates coming from ens33 the outside facing interface
-	toWrite=$seconds','$RX_TX','$Disk_Write','$Avail_Space
+	toWrite=$seconds','$RX','$TX','$Disk_Write','$Avail_Space
 	echo $toWrite >> System_Metrics.csv
 }
 
-ipToUse=192.168.195.129
+#ipToUse=192.168.195.129
 #hostname -I | sed 's/\s.*$//'
+ipToUse=`hostname -I | awk '{print $1}'`
+
 echo $ipToUse
 function RunC() {
 	./APM1 $ipToUse &
@@ -55,7 +62,7 @@ function RunC() {
 	#Starts all of the C programs
 }
 function Kill() {
-	killall APM*
+	killall APM* > /dev/null 2>&1
 	#Kills all of the APM named processes
 }
 
@@ -66,20 +73,22 @@ fi
 }
 
 RunC
-end=$((SECONDS+60))
+end=$((SECONDS+30))
 #Running for exactly 15 mins
 while [ $SECONDS -lt $end ];
 do
-	echo "Collecting"
-	collect
+	#echo "Collecting"
+	#collect
 	#Sleep 4 because it takes about a second to run
-	sleep 4
+	#sleep 4
 
 	#Attempted to make it run when Seconds % 5 was 0, did not work.
 	#echo "Collecting"
-	#if [ $(($SECONDS%5)) -eq 0 ]
-		#then collect
-	#fi
+	funf=$((SECONDS%5))
+	if [ $funf -eq 0 ]; then 
+           echo "$SECONDS"
+           collect
+	   sleep 1
+	fi
 done
 Kill
-
